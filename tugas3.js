3d029edf6dfa58f79509d4d1a4f11db8cdb03af6
Cahyo Novianto <cahyonovianto97@gmail.com>
2022-11-18 14:26:46 +0700

    mengubah file tugas3.js

--- tugas3.js
+++ tugas3.js
@@ -1,22 +1,22 @@
-function arrayCheck(firstValue, lastValue, arrayData){
-    arrayData = [];
-    if (array.length < 5){
+function arrayCheck(firstValue, lastValue){
+    const arrayData = [1,2,3,4,5];
+    if (arrayData.length < 5){
         console.log("jumlah data array kurang")
     }
-    for (let i = 0; i <= array.length; i++){
-            if(array[i] === firstValue || array[i] === lastValue ){
+    for (let i = 0; i <= arrayData.length; i++){
+            if(arrayData[i] === firstValue || arrayData[i] === lastValue ){
                 console.log("tidak ada data")
             } 
     }
 
     if (firstValue < lastValue){
-        return array.sort()
+        return arrayData.sort()
     } else {
         console.log("data salah")
     }
 
 
 }
-arrayCheck(1 , 8, [1,2,3,4,5]);
+arrayCheck(1 , 8);
 
     
