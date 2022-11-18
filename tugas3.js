4c5b427f98cf0091aea7aec9becc512c0e415783
Cahyo Novianto <cahyonovianto97@gmail.com>
2022-11-18 14:51:10 +0700

    memperbaiki file tugas3.js 1

--- tugas3.js
+++ tugas3.js
@@ -1,19 +1,18 @@
 function arrayCheck(firstValue, lastValue, arrayData){
+    for (let i = 0; i <= arrayData.length; i++)
+    
     if (arrayData.length < 5){
-        return "jumlah data array kurang"
-        
-    }
-    for (let i = 0; i <= arrayData.length; i++){
-            if(arrayData[i] === firstValue && arrayData[i] === lastValue ){
-                console.log("tidak ada data")
-            } else if (firstValue < lastValue){
+        console.log("jumlah data kurang")
+        if(arrayData[i] === firstValue && arrayData[i] === lastValue ){
+            console.log("tidak ada data")
+        }
+            else if (firstValue < lastValue){
                 return arrayData.sort()
-            }
-             else {
+            }  else {
                 console.log("Nilai akhir harus lebih besar dari nilai awal")
-            }
-        
+            }      
     }
+    
 
     // if (firstValue < lastValue){
     //     return arrayData.sort()
