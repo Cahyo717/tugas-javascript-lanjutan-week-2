b740a9866ef2729949fedaf125e39d1a3c21c2c6
Cahyo Novianto <cahyonovianto97@gmail.com>
2022-11-18 14:38:00 +0700

    menghapus arrayData = []

--- tugas3.js
+++ tugas3.js
@@ -1,22 +1,25 @@
-function arrayCheck(firstValue, lastValue){
-    const arrayData = [1,2,3,4,5];
+function arrayCheck(firstValue, lastValue, arrayData){
     if (arrayData.length < 5){
         console.log("jumlah data array kurang")
+        
     }
     for (let i = 0; i <= arrayData.length; i++){
             if(arrayData[i] === firstValue || arrayData[i] === lastValue ){
                 console.log("tidak ada data")
             } 
+        
     }
 
     if (firstValue < lastValue){
         return arrayData.sort()
     } else {
-        console.log("data salah")
+        console.log("nilai akhir harus lebih besar dari nilai awal")
     }
+
+  
 
 
 }
-arrayCheck(1 , 8);
+arrayCheck(15 , 8, [1,2,3,5]);
 
     
